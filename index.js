1123a4c08b57e326a152cb639cd58a4498fd6928
Rafa Laguna <rafa.laguna@enti.cat>
2025-01-28 19:50:06 +0100

    Sync game states

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ let ws_server = new ws.Server({server: http_server});
 
 let player1, player2;
 
+
 ws_server.on('connection', function (conn) {
 
 	console.log("Usuario conectado");
@@ -35,6 +36,12 @@ ws_server.on('connection', function (conn) {
 		};
 
 		player1.send( JSON.stringify(info)  );
+
+		player1.on('close', function () {
+			console.log("Player 1 disconnected");
+			player1 = null;
+		});
+
 
 		player1.on('message', function (msg) {
 			if (player2 == null)
@@ -51,6 +58,26 @@ ws_server.on('connection', function (conn) {
 			else if (info.score1 != null) {
 
 				player2.send( JSON.stringify(info) );
+				
+				if (info.score1 >= 3 || info.score2 >= 3){
+					
+					let data = {
+						game_over: true,
+						winner: 0
+					};
+
+					if (info.score1 >= 3)
+						data.winner = 1;
+					else
+						data.winner = 2;
+
+					let data_json = JSON.stringify(data); 
+
+					player1.send( data_json );
+					player2.send( data_json );
+
+					return;
+				}
 
 			}
 		});
@@ -63,6 +90,29 @@ ws_server.on('connection', function (conn) {
 		};
 
 		player2.send( JSON.stringify(info)  );
+
+		player2.on('close', function () {
+			console.log("Player 2 disconnected");
+			player2 = null;
+		});
+
+
+
+
+		setTimeout(function() {
+			let info = {
+				game_start: true
+			};
+
+			let info_json = JSON.stringify(info); 
+
+			player1.send( info_json );
+			player2.send( info_json );
+		}, 500);
+
+
+
+
 
 		player2.on('message', function (msg) {
 			if (player1 == null)
@@ -77,7 +127,4 @@ ws_server.on('connection', function (conn) {
 		//	console.log("Jugador 2: "+msg);
 		});
 	}
-
-
-
 });
